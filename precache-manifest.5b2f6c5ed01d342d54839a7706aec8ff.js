86ceea6ed6eda7a01adc80159a93552165fae3cd
JonathanB450 <jonathanbuitragoroncancio50@gmail.com>
2020-02-10 23:03:03 -0500

    Updates

---
+++ precache-manifest.5b2f6c5ed01d342d54839a7706aec8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541e32ec68b5532883bb17fdab537329",
+    "url": "/horario/index.html"
+  },
+  {
+    "revision": "f13c776ad8ff3f3a3b7c",
+    "url": "/horario/static/css/main.550d99d4.chunk.css"
+  },
+  {
+    "revision": "85d15b05e4383a27fe2c",
+    "url": "/horario/static/js/2.d3a134c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/horario/static/js/2.d3a134c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13c776ad8ff3f3a3b7c",
+    "url": "/horario/static/js/main.4fef75b8.chunk.js"
+  },
+  {
+    "revision": "a81c2a34717a9e13ccad",
+    "url": "/horario/static/js/runtime-main.612b301b.js"
+  },
+  {
+    "revision": "292f164bf8a4ae23ac51b0ceb632cb64",
+    "url": "/horario/static/media/calculadora.292f164b.svg"
+  }
+]);
